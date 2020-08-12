0b636c9ebb28ee8e3d7be49efcb52ae0188cd3dc
gakubochan <gaqbochan@gmail.com>
2020-08-13 00:13:19 +0900

    first commit

---
+++ binary_law.py
@@ -0,0 +1,18 @@
+import math
+import numpy as np
+def f(x):
+    return math.cos(x)-x
+
+a=0
+b=1
+n=0
+while n<=10:
+    c=(a+b)/2
+    if np.sign(f(c))==np.sign(f(a)):
+        a=c
+    else: b=c
+    n=n+1
+print(c)
+
+
+
